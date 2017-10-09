17f50b2b0c788420640ab9dd31077be64c0eb6e2
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-10-08 13:07:16 -1100

    Update SSDS.py

--- SSDS.py
+++ SSDS.py
@@ -26,10 +26,67 @@ H_query  = ["how", "how long", "how have"]
 # domain_sensitive_words = ['go', 'price', 'travel', 'trip', 'flight', 'ticket', 'check', 'buy', 'one way', 'round trip', 'class', 'economy', ...]
 # scope_word             = ['for', 'from', 'to', 'toward', 'destination', 'depart', 'in', ...]
 
+# domain["price"]           = ['price', 'how much', 'cost']
+# domain["available"]       = ['free', 'available', 'possible']
+
+idiom = ["Can you", "You gotta", ...]
+
+#Defining incremental information function : set everything with None at first.
+def check(domian, city_from=None, city_to=None, date1=None, date2=None, info=None):
+    data     = search(city_from=None, city_to=None, date1=None, date2=None, info=None) #from somewhere.
+    
+    price, available, weather, temperature, ... = data
+    
+    if   domain=="price"       : return price
+    elif domain=="available"   : return available
+    elif domain=="temperature" : return temperature
+    else : raise ValueError()
+
+def search(city_from=None, city_to=None, date1=None, date2=None, info=None):
+    cf, ct, d1, d2 = city_from, city_to, date1, date2
+    
+    #Go to recursive, deeper with extracted information.
+    if cf or ct or d1 or d2 : return check(info=Extract_manually(utterence))
+    else:
+        #if there is no Start, Destination info.
+        if not cf and not ct :
+            make_it_utterenace(idiom[random.randint]+"[Can you] tell me where to, or where from?")
+        else:
+            if not d1 and not d2:
+                make_it_utterenace(idiom[random.randint]+"[You gotta] tell me when you want to go. or arrive")
+            else:
+                return get_info(cf, ct, d1, d2)
+                #get_info returns price, available, weather, temperature...
+
+def get_info(cf, ct, d1, d2):
+    price       = get_price_api(cf, ct, d1, d2)
+    available   = check_free_seat(cf, ct, d1, d2)
+    #temperature = @TODO
+
+def reprompt():
+    reprompt_list = ["what did you say", "Can you tell me once again", "Pardon?",]
+    return random.randint(0, len(reprompt_list))
+        
 def intepret(utterance):
     words = utterence.split(' ')
-    word.remove(All_adjective)
-
+    words.remove(All_adjective)
+    
+    #Here, need to use "scope_word" for find relation between two city (if there are two cities given)
+    given_city = list()
     for word in words:
-        if word in wh_query:
-            
+        for city_ in city:
+            if word==city_ : given_city.append(city_)
+    
+    #scope, order is matter, "for" would indicate both, "to" tells destination, "from" gives origin...
+    #TODO, make city order by scope.
+    
+    
+    for word in words:
+        if word in domain_sensitive_words:
+            if   word in domain["price"]:
+                return check("price", city_from=None, city_to=None, date1=None, date2=None)
+            elif word in domain["available"]:
+                return check("available", city_from=None, city_to=None, date1=None, date2=None)
+            #elif...
+            else :
+                return reprompt()
